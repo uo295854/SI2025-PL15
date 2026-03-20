cb03406ddc619218f7c3645ab2d025cb989f9e10
daraf <daraf@DESKTOP-BPP97N7>
2026-03-20 12:38:07 +0100

    poner los nombres de días al español

--- src/main/java/giis/sisinfo/model/ReservaInstalacionAdminSocioModel.java
+++ src/main/java/giis/sisinfo/model/ReservaInstalacionAdminSocioModel.java
@@ -126,7 +126,7 @@ public class ReservaInstalacionAdminSocioModel {
 			else
 				estado = diaCompleto(idInstalacion, fecha) ? "COMPLETO" : "DISPONIBLE";
 
-			resultado.add(new DiaReservaSocioDTO(fecha.getDayOfWeek().toString(), fecha.toString(), estado));
+			resultado.add(new DiaReservaSocioDTO(diaSemana(fecha), fecha.toString(), estado));
 		}
 		return resultado;
 	}
@@ -412,6 +412,19 @@ public class ReservaInstalacionAdminSocioModel {
 				+ "VALUES (?,?,?,?,?,?)";
 		
 		db.executeUpdate(sql, idSocio, idReservaIns, importe, "RESERVA", LocalDate.now().toString(), estadoPago);
+	}
+	
+	private String diaSemana(LocalDate fecha) {
+	    switch (fecha.getDayOfWeek()) {
+	        case MONDAY: return "Lunes";
+	        case TUESDAY: return "Martes";
+	        case WEDNESDAY: return "Miércoles";
+	        case THURSDAY: return "Jueves";
+	        case FRIDAY: return "Viernes";
+	        case SATURDAY: return "Sábado";
+	        case SUNDAY: return "Domingo";
+	        default: return "";
+	    }
 	}
 	
 }
