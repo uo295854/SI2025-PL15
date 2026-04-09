5c6c933a4275f831f75d6a62725393d93bfebdad
Artic <Artic@DESKTOP-34ALROM>
2026-04-09 09:09:49 +0200

    Pequeño avance en la interfaz de ListadoCargosMensualesSocio

--- src/main/java/giis/sisinfo/view/ListadoCargosMensualesSocioView.java
+++ src/main/java/giis/sisinfo/view/ListadoCargosMensualesSocioView.java
@@ -8,13 +8,22 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JComboBox;
 import javax.swing.JButton;
 import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JTextField;
+import javax.swing.JTextPane;
+import javax.swing.DefaultComboBoxModel;
 
 public class ListadoCargosMensualesSocioView extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JTable table;
-
+	JComboBox selectorMes;
+	JComboBox selectorAño;
+	JComboBox selectorCargos;
+	JButton botonBuscar;
+	
+	
 	/**
 	 * Launch the application.
 	 */
@@ -36,27 +45,70 @@ public class ListadoCargosMensualesSocioView extends JFrame {
 	 */
 	public ListadoCargosMensualesSocioView() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 708, 446);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JComboBox comboBox = new JComboBox();
-		comboBox.setBounds(10, 10, 65, 20);
-		contentPane.add(comboBox);
+		selectorMes = new JComboBox();
+		selectorMes.setModel(new DefaultComboBoxModel(new String[] {"2024", "2025", "2026", "2027", "2028", "2029", "2030", "2031", "2032"}));
+		selectorMes.setBounds(55, 20, 80, 20);
+		contentPane.add(selectorMes);
 		
-		JComboBox comboBox_1 = new JComboBox();
-		comboBox_1.setBounds(93, 10, 70, 20);
-		contentPane.add(comboBox_1);
+		selectorAño = new JComboBox();
+		selectorAño.setModel(new DefaultComboBoxModel(new String[] {"Enero", "Febrero", "Marzo", "Junio", "Julio", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Noviembre", "Diciembre"}));
+		selectorAño.setBounds(228, 20, 70, 20);
+		contentPane.add(selectorAño);
 		
-		JButton botonBuscar = new JButton("Buscar");
-		botonBuscar.setBounds(194, 10, 92, 20);
+		botonBuscar = new JButton("Buscar");
+		botonBuscar.setBounds(537, 10, 106, 40);
 		contentPane.add(botonBuscar);
 		
 		table = new JTable();
-		table.setBounds(10, 40, 283, 190);
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null, null, null, null, null},
+				{null, null, null, null, null},
+				{null, null, null, null, null},
+				{null, null, null, null, null},
+			},
+			new String[] {
+				"New column", "New column", "New column", "New column", "New column"
+			}
+		));
+		table.setBounds(10, 69, 633, 282);
 		contentPane.add(table);
+		
+		selectorCargos = new JComboBox();
+		selectorCargos.setBounds(418, 19, 67, 22);
+		contentPane.add(selectorCargos);
+		
+		JTextPane txtpnMes = new JTextPane();
+		txtpnMes.setEditable(false);
+		txtpnMes.setText("Mes");
+		txtpnMes.setBounds(182, 20, 44, 20);
+		contentPane.add(txtpnMes);
+		
+		JTextPane txtpnAnyo = new JTextPane();
+		txtpnAnyo.setText("Año");
+		txtpnAnyo.setEditable(false);
+		txtpnAnyo.setBounds(10, 20, 44, 20);
+		contentPane.add(txtpnAnyo);
+		
+		JTextPane txtpnCargos = new JTextPane();
+		txtpnCargos.setText("Cargos");
+		txtpnCargos.setEditable(false);
+		txtpnCargos.setBounds(332, 20, 44, 20);
+		contentPane.add(txtpnCargos);
 
+	}
+
+	public JTable getTable() {
+		return table;
+	}
+
+	public void setTable(JTable table) {
+		this.table = table;
 	}
 }
